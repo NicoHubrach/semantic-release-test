07ea2b72b58db65c6cc20958a3bfdb21de24edd3
Nico Hubrach <nico.hubrach@gmx.de>
2025-04-06 20:11:37 +0200

    feat: automatic ci/cd

---
+++ release.config.mjs
@@ -0,0 +1,7 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+    branches: ["master"],
+    plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/github"],
+};
