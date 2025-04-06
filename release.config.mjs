6ab93f47650251cdd0cdf467a930b4c951fec104
Nico Hubrach <nico.hubrach@gmx.de>
2025-04-06 20:31:27 +0200

    fix: Automatic Export

--- release.config.mjs
+++ release.config.mjs
@@ -3,5 +3,5 @@
  */
 export default {
     branches: ["master"],
-    plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/github"],
+    plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/github", "@semantic-release/changelog"],
 };
